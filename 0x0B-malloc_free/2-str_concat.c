1cce20c23a26c3d33709718c1de4aa0a64f167bf
RamathabatheN <mashoto23@gmail.com>
2023-09-05 04:47:08 -0700

    free malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concatenate
+ * @s2: input two to concatenate
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *mix;
+int i, j;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+i = j = 0;
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
+mix = malloc(sizeof(char) * (i + j + 1));
+if (mix == NULL)
+return (NULL);
+i = j = 0;
+while (s1[i] != '\0')
+{
+mix[i] = s1[i];
+i++;
+}
+while (s2[j] != '\0')
+{
+mix[i] = s2[j];
+i++, j++;
+}
+mix[i] = '\0';
+return (mix);
+}
