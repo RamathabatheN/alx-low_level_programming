e759778c80f701c7210eff0713fa0016e8d9b808
RamathabatheN <mashoto23@gmail.com>
2023-09-22 04:51:44 -0700

    linked lits

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t v = 0;
+size_t s = 0;
 while (h)
 {
 if (!h->str)
@@ -16,7 +16,7 @@ printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-v++;
+s++;
 }
-return (v);
+return (s);
 }
