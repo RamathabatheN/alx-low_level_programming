bd3315286f51e4ef38b45e29b38abcf764c4c022
RamathabatheN <mashoto23@gmail.com>
2023-09-25 15:28:48 -0700

    argv argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,31 +9,31 @@
  */
 int _atoi(char *s)
 {
-int i, d, n, length, f, dig;
-i = 0;
-d = 0;
+int a, b, n, length, f, dig;
+a = 0;
+b = 0;
 n = 0;
 length = 0;
 f = 0;
 dig = 0;
 while (s[length] != '\0')
 length++;
-while (i < length && f == 0)
+while (a < length && f == 0)
 {
-if (s[i] == '-')
-++d;
-if (s[i] >= '0' && s[i] <= '9')
+if (s[a] == '-')
+++b;
+if (s[a] >= '0' && s[a] <= '9')
 {
-dig = s[i] - '0';
-if (d % 2)
+dig = s[a] - '0';
+if (b % 2)
 dig = -dig;
 n = n * 10 + dig;
 f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
+if (s[a + 1] < '0' || s[a + 1] > '9')
 break;
 f = 0;
 }
-i++;
+a++;
 }
 if (f == 0)
 return (0);
@@ -57,7 +57,7 @@ return (1);
 }
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n , result");
+result = num1 *num2;
+printf("%d\n" ,result);
 return (0);
 }
