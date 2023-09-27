16843b45ef5fb641cf889e260e1726187ed1e239
RamathabatheN <mashoto23@gmail.com>
2023-09-27 04:55:28 -0700

    free malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *prey;
+char *ptr1;
 char *old_ptr;
 unsigned int i;
 if (new_size == old_size)
@@ -23,20 +23,20 @@ return (NULL);
 }
 if (!ptr)
 return (malloc(new_size));
-prey = malloc(new_size);
-if (!prey)
+ptr1 = malloc(new_size);
+if (!ptr1)
 return (NULL);
 old_ptr = ptr;
 if (new_size < old_size)
 {
 for (i = 0; i < new_size; i++)
-prey[i] = old_ptr[i];
+ptr1[i] = old_ptr[i];
 }
 if (new_size > old_size)
 {
 for (i = 0; i < old_size; i++)
-prey[i] = old_ptr[i];
+ptr1[i] = old_ptr[i];
 }
 free(ptr);
-return (prey);
+return (ptr1);
 }
